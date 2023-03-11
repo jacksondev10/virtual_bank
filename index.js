0978c3058b2819a085425e7dd057427beeb43f53
Jackson Santos <jacksf1210@gmail.com>
2023-03-11 11:11:08 -0300

    Faça seu cadastro

--- index.js
+++ index.js
@@ -15,5 +15,55 @@ function operation(){
         message: 'O que você deseja fazer?',
         choices: ['Criar conta','Consultar Saldo','Depositar','Sacar','Sair'],      
     },
-]).then().catch((err) => console.log(err))
+]).then((answer) => {
+    const action = answer['action']
+if (action === 'Criar conta'){
+    creationAccount()
+}
+    
+})
+.catch((err) => console.log(err))
+}
+
+// Criar conta
+function creationAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
+    return
+}
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name:'accountName',
+            message: 'Digite um nome para a sua conta:',
+        },
+    ])
+    .then((answer) => {
+       const accountName = answer['accountName']
+
+       console.info(accountName)
+
+        if (!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Usuario já existe!')
+            )
+            buildAccount()
+
+        }       
+    fs.writeFileSync(`accounts/${accountName}.json`,
+     '{"balance": 0}',
+     function (err) {
+        Console.log(err)
+     },
+     )
+     console.log(chalk.green('Sua conta foi criada com sucesso!'))
+     operation()
+    })
+    .catch(err => console.log(err))
 }
